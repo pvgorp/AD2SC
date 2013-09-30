8c09eb5c292ab87bc7742a223ca39497e8886023
git@pietervangorp.com <git@pietervangorp.com>
2013-09-30 17:30:35 +0200

    Incremental step towards removal of bug for claim4 example

---
+++ ad2grgen/src/run - claim4.bat
@@ -0,0 +1,1 @@
+java -cp ..\lib\jdom.jar;..\bin ad2grgen ..\..\grgen2sc\input\claim4.uml.xml ..\..\grgen2sc\input\claim4.uml.gruml23
---
+++ ad2grgen/src/run - shipping.bat
@@ -0,0 +1,1 @@
+java -cp ..\lib\jdom.jar;..\bin ad2grgen ..\..\grgen2sc\input\shipping-DAB.xml ..\..\grgen2sc\input\shipping-DAB.gruml23
