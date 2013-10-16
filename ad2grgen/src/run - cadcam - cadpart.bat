6cd6a0bc95cf93c1dd194f1982a44973a47039b4
Pieter Van Gorp <git@pietervangorp.com>
2013-10-16 14:46:43 +0200

    Converted old testcases
    Observation: datawarehouse example needs rework

---
+++ ad2grgen/src/run - cadcam - cadpart.bat
@@ -0,0 +1,1 @@
+java -cp ..\lib\jdom.jar;..\bin ad2grgen ..\..\grgen2sc\input\cadcam-cadpart.uml.xml ..\..\grgen2sc\input\cadcam-cadpart.uml.gruml23
---
+++ ad2grgen/src/run - catalogue.bat
@@ -0,0 +1,1 @@
+java -cp ..\lib\jdom.jar;..\bin ad2grgen ..\..\grgen2sc\input\catalogue-repaired.uml.xml ..\..\grgen2sc\input\catalogue.uml.gruml23
